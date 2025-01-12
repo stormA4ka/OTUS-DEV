2c7b135df6900d66becb42cb23beb4a62e3ad3d9
stormA4ka <stm.87@mail.ru>
2025-01-12 20:41:02 +0300

    Delete lesson.06 directory

--- lesson.06/my_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect
-from .models import Application
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Добро пожаловать на главную страницу!")
