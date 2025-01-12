f6383cc4977997ff15332c964fda4f71d6e1d79c
Alexander Derdilev <stm.87@mail.ru>
2025-01-13 00:39:53 +0300

    hw06 edit model, readme

--- lesson.06/my_app/models.py
+++ lesson.06/my_app/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.utils import timezone
 
-class Application(models.Model):
+class Users(models.Model):
     # Обязательные поля
-    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
+    phone_number = models.CharField(max_length=20, unique=True, verbose_name="Номер телефона")
+    
+    # Необязательные поля
+    first_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="Фамилия")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.phone_number})"
+
+class Task(models.Model):
+    # Обязательные поля
+    phone_number = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='tasks', verbose_name="Номер телефона")
     platform_type = models.CharField(max_length=100, verbose_name="Тип площадки")
     legal_entity = models.CharField(max_length=100, verbose_name="Юрлицо")
     problem_description = models.TextField(verbose_name="Краткое описание проблемы")
     problem_location = models.CharField(max_length=200, verbose_name="Место проблемы")
     problem_type = models.CharField(max_length=100, verbose_name="Тип проблемы")
-    application_date = models.DateTimeField(default=timezone.now, verbose_name="Дата заявки")
-    application_id = models.AutoField(primary_key=True, verbose_name="Номер заявки ID")
+    task_date = models.DateTimeField(default=timezone.now, verbose_name="Дата заявки")
+    task_id = models.AutoField(primary_key=True, verbose_name="Номер заявки ID")
 
     # Необязательные поля
     problem_photo = models.ImageField(upload_to='problem_photos/', blank=True, null=True, verbose_name="Фото проблемы")
@@ -22,4 +33,4 @@ class Application(models.Model):
     planned_fix_date = models.DateField(blank=True, null=True, verbose_name="Плановая дата устранения")
 
     def __str__(self):
-        return f"Заявка #{self.application_id}"
+        return f"Заявка #{self.task_id}"
--- lesson.06/my_app/views.py
+++ lesson.06/my_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from .models import Application
 from django.http import HttpResponse
 
 def home(request):
